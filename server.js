e5f3942814ba1b36a9c474f0df741a886a698f53
heathercoraje <pancontomateyjamon18@gmal.com>
2020-02-15 19:36:17 +0000

    .get /users & .post /new-user

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -5,7 +7,11 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+const Schema = mongoose.Schema;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true
+});
 
 app.use(cors())
 
@@ -18,30 +24,52 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// Schema/model setup 
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  exercise: { type: Array, required: false }
+});
 
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
+const User = mongoose.model('users', userSchema);
 
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
+app.get('/api/exercise/users', (req, res, next) => {
+  User.find({}, (error, data) => {
+    if (error || !data.length) res.json({ error: "No user exist"});
+    else res.send(data);
+  });
+});
 
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
+app.post('/api/exercise/new-user', (req, res, next) => {
+  const username = req.body.username;
+  const user = new User({
+    username: username
+  });
+  
+  User.find({ username: username }, (error, document) => { // TODO REFACTOR
+    if (error) res.json({ error: "Error while finding this user"});
+    if (document.length) res.json({ error: "Username already exist, please choose a differnet username"});
+    else {
+      user.save((error, savedDocument) => {
+        if (error) res.json({ error: "Error while saving a user"});
+        res.json({ username: savedDocument.username, id: savedDocument._id})
+      });
+    }
+  });
+});
+
+app.get('/api/exercise/log/:userId', (req, res, next) => {
+  // retrive a full log of exercise 
+  // a full exercise log of user object with added array log and count // // (total exercise count).
+ });
+
+app.get('/api/exercise/log/:userId', (req, res, next) => {
+   // retrieve part of the log
+  });
+  
+app.post('/api/exercise/add', (req, res, next) => {
+  // add exercise document to excercise collection
+  // return json of user object with exercise fields added
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
