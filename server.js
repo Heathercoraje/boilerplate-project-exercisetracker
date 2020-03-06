b3b571d5f216a771f34927b47d3c0d32008e9c74
heathercoraje <pancontomateyjamon18@gmal.com>
2020-03-06 13:41:13 +0000

    add post /api/exercise/add

--- server.js
+++ server.js
@@ -25,10 +25,12 @@ app.get('/', (req, res) => {
 });
 
 // Schema/model setup 
+//  move to DB file
 const userSchema = new Schema({
   username: { type: String, required: true },
   exercise: { type: Array, required: false }
 });
+
 
 const User = mongoose.model('users', userSchema);
 
@@ -45,7 +47,7 @@ app.post('/api/exercise/new-user', (req, res, next) => {
     username: username
   });
   
-  User.find({ username: username }, (error, document) => { // TODO REFACTOR
+  User.find({ username: username }, (error, document) => {
     if (error) res.json({ error: "Error while finding this user"});
     if (document.length) res.json({ error: "Username already exist, please choose a differnet username"});
     else {
@@ -57,19 +59,64 @@ app.post('/api/exercise/new-user', (req, res, next) => {
   });
 });
 
-app.get('/api/exercise/log/:userId', (req, res, next) => {
-  // retrive a full log of exercise 
-  // a full exercise log of user object with added array log and count // // (total exercise count).
+app.post('/api/exercise/add', (req, res, next) => {
+  let user;
+  let newExerciseArray = [];
+
+  const { userId, description, duration, date } = req.body;
+  const newExercise = { userId, date, description, duration };
+  
+  User.find({_id: userId}, (error, document) => {
+    if (error) res.json({Error: "Error finding user with userId" });
+    else {
+      user = document[0]
+      newExerciseArray.push(newExercise);
+
+      if (user.exercise) {
+        user.exercise.map((exercise) => newExerciseArray.push(exercise));
+      }
+
+      const objectToUpdate = { exercise: newExerciseArray };
+      User.findByIdAndUpdate( userId, objectToUpdate , { new: true }, (error, document) => {
+        if (error) res.json({ Error: "Error findAndUpdating user with userId" });
+        else {
+          res.json(document);
+        }
+      });
+    }
+  });
+});
+
+app.get('/api/exercise/log', (req, res, next) => {
+  const { userId } = req.query; 
+  let userObject;
+  console.log('userId', req.query.userId);
+
+  User.find({ _id: userId }, (error, document) => {
+    if (error) res.json({ error: "Error while finding this user"});
+    if (!document.length) res.json({ error: `No user exist with userId: ${userId}`});
+    else {
+      console.log('.find', document);
+      const log = document.exercise;  
+      
+      userObject = {
+        userId,
+        log: log ? log : [],
+        count: log ? log.length: 0
+      }  
+      console.log('find->userObject', userObject);
+      res.json(userObject)
+    }
+  });
  });
 
+
 app.get('/api/exercise/log/:userId', (req, res, next) => {
-   // retrieve part of the log
+   // retrieve part of the log 
+   // extra param
+   // use the original route
   });
   
-app.post('/api/exercise/add', (req, res, next) => {
-  // add exercise document to excercise collection
-  // return json of user object with exercise fields added
-});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
